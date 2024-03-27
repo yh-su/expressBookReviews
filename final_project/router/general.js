3ed77f08aa8f40bedc0a590a40ac64137e2d50fe
Yu Heng Su <yolanda.su00@gmail.com>
2024-03-27 18:18:56 -0400

    Completed Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn], null, 4));
  });
   
 // Get book details based on author
