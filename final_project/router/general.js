b82405d64b668f6cacc4233fa30cefe0ee7563e6
Yu Heng Su <yolanda.su00@gmail.com>
2024-03-28 18:37:24 -0400

    Completed Tasks 3-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,20 +23,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let filtered_books = {};
+    Object.keys(books).forEach(function(key) {
+    if(books[key]["author"] == req.params.author) {
+        filtered_books[key] = books[key];
+    }
+  })
+  return res.send(JSON.stringify(filtered_books, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let filtered_books = {};
+    Object.keys(books).forEach(function(key) {
+    if(books[key]["title"] == req.params.title) {
+        filtered_books[key] = books[key];
+    }
+  })
+  return res.send(JSON.stringify(filtered_books, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn]["reviews"], null, 4));
 });
 
 module.exports.general = public_users;
