64f0841912728fcf8bea15e2873a8f6eb1960590
Yu Heng Su <yolanda.su00@gmail.com>
2024-04-02 18:09:43 -0400

    Completed Tasks 8-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,9 +47,29 @@ regd_users.post("/login", (req,res) => {
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn", (req, res) => { 
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization['username'];
+    if(!books[isbn]) {
+        return res.status(404).json({message: "No book exists for given ISBN ${isbn}"});
+    }
+    books[isbn]["reviews"][username] = review;
+    return res.status(200).json({message: "Review successfully added"});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => { 
+    const isbn = req.params.isbn;
+    const username = req.session.authorization['username'];
+    if(!books[isbn]) {
+        return res.status(404).json({message: "No book exists for given ISBN ${isbn}"});
+    }
+    if (!books[isbn]["reviews"][username]) {
+        return res.status(404).json({message: "No review to delete"});
+    }
+    delete books[isbn]["reviews"][username];
+    return res.status(200).json({message: "Review successfully deleted"});
 });
 
 module.exports.authenticated = regd_users;
